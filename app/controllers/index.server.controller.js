b8466d75b640e7c46d8ead7d680bfaa1d42eeab5
darknawg63@gmail.com <darknawg63@gmail.com>
2016-05-18 19:27:28 +0200

    Configuring the view system

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,6 @@
 exports.render = function(req, res) {
-    res.send('Hello World\n');
+    //res.send('Hello World\n');
+    res.render('index', {
+        title: 'Hello World'
+    })
 };
--- config/express.js
+++ config/express.js
@@ -10,11 +10,17 @@ var express = require('express'),
         } else if (process.env.NODE_ENV === 'production') {
             app.use(compress());
         }
+
         app.use(bodyParser.urlencoded({
             extended: true
         }));
+
         app.use(bodyParser.json());
         app.use(methodOverride());
+
+        app.set('views', './app/views');
+        app.set('view engine', 'ejs');
+
         require('../app/routes/index.server.routes.js')(app);
         return app;
     };
